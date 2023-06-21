5c84b58538c5748da04e21bc5a6f8fe4b76519ee
teliej <teliej52@gmail.com>
2023-06-21 15:24:14 -0700

    last

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @i: int to be added to j
+ * @j: int to be added to i
+ * Return: sum of i and j
+ */
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+	return (k);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int x;
+
+    x = add(89, 9);
+    printf("%d\n", x);
+    return (0);
+}
