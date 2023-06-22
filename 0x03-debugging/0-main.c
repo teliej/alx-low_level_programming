47206a40fa03cad60cb6b35dfbce0e94097a4b12
teliej <teliej52@gmail.com>
2023-06-22 15:29:42 -0700

    loki

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests if number is positive or negative
+ * Return: success (0)
+ */
+int main(void)
+{
+
+	int x;
+
+	x = 0;
+	positive_or_negative(x);
+	return (0);
+}
