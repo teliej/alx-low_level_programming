625e988728ef8f576833e028b3694639397e1ed3
teliej <teliej52@gmail.com>
2023-06-26 12:58:00 -0700

    first pro commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - takes a pointer as a int parameter
+ * and updates the value it points to 98
+ *
+ * @x: int parameter
+ *
+ * Return: Nothing
+ */
+void reset_to_98(int *x)
+{
+	*x = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+just a simple readme for this project
